65410f946d4cc1be8203de43e57edae438b917a5
deshankoswatte <dehami.deshan@gmail.com>
2020-01-07 12:45:31 +0530

    Initial changes for OpenSAML3

--- components/org.wso2.carbon.identity.entitlement.proxy/src/main/org/wso2/carbon/identity/entitlement/proxy/wsxacml/X509CredentialImpl.java
+++ components/org.wso2.carbon.identity.entitlement.proxy/src/main/org/wso2/carbon/identity/entitlement/proxy/wsxacml/X509CredentialImpl.java
@@ -20,10 +20,10 @@
 
 package org.wso2.carbon.identity.entitlement.proxy.wsxacml;
 
-import org.opensaml.xml.security.credential.Credential;
-import org.opensaml.xml.security.credential.CredentialContextSet;
-import org.opensaml.xml.security.credential.UsageType;
-import org.opensaml.xml.security.x509.X509Credential;
+import org.opensaml.security.credential.Credential;
+import org.opensaml.security.credential.CredentialContextSet;
+import org.opensaml.security.credential.UsageType;
+import org.opensaml.security.x509.X509Credential;
 
 import javax.crypto.SecretKey;
 import java.math.BigInteger;
@@ -97,7 +97,7 @@ public class X509CredentialImpl implements X509Credential {
     }
 
     @Override
-    public CredentialContextSet getCredentalContextSet() {
+    public CredentialContextSet getCredentialContextSet() {
         return null;
     }
 
